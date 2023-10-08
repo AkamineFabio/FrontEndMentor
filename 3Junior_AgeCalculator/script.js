ee30371a34cbf37b84b9b0fe219e16ca7e98523f
AkamineFabio <pokeevo43@gmail.com>
2023-10-08 05:50:43 -0300

    update css

--- 3Junior_AgeCalculator/script.js
+++ 3Junior_AgeCalculator/script.js
@@ -14,16 +14,22 @@ const yearsNumberText = document.querySelector('#number__years');
 const monthsdays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 const ONE_DAY_TIME_STAMP = 1000 * 60 * 60 * 24; //miliseconds inside secs * secs inside minutes * minutes inside hours * hours inside day
 
+let daysUser = 0; //days of life user has
+
+let monthsAge = 0; //months user has, not counting years -> to use in text
+let yearsAge = 0; //years user has -> to use in text
+let daysAge = 0; //days user has, not counting years or months -> to use in text
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let dateToday = new Date();
     let dateBirthday = new Date(inputYear.value, inputMonth.value - 1, inputDay.value);
     let userAge = new Date();
 
-    let daysUser = 0; //days of life user has
-    let monthsAge = 0; //months user has, not counting years
-    let yearsAge = 0; //years user has
-    let daysAge = 0; //days user has, not counting years or months
+    daysUser = 0;
+    monthsAge = 0;
+    yearsAge = 0;
+    daysAge = 0;
 
     let daysLeft = 0; //days user has, variable for hold the value of total days in the actual year since user's birthday
 
@@ -33,9 +39,7 @@ form.addEventListener('submit', (e) => {
         userLeapYearQtd++; //add one, cause the birth year has one more day
     }
 
-    console.log(`TODAY: ${dateToday.getTime()}`);
-    console.log(`BIRTH DATE: ${dateBirthday.getTime()}`);
-    userAge.setTime(dateToday.getTime() - dateBirthday.getTime());
+    userAge.setTime(dateToday.getTime() - dateBirthday.getTime()); //difference between these dates will result in how old user is, in milisecs
 
     daysUser = parseInt(userAge.getTime() / ONE_DAY_TIME_STAMP); //miliseconds of life user has, divided by miliseconds in one day -> this will give me how many days user has
     yearsAge = parseInt(daysUser / 365); // days user has divided by how many days a year has, resulting in how many years user
@@ -45,7 +49,6 @@ form.addEventListener('submit', (e) => {
     daysLeft = parseInt(daysUser - (365 * yearsAge)) - userLeapYearQtd; //how many days user has in the actual year, and removing the days that I didn't count above, cause some years has 366 days, and I used 365
 
     for (let i = 0; i < monthsdays.length; i++) {
-        console.log('Days left' + daysLeft);
         daysLeft = daysLeft - monthsdays[i];
         monthsAge++;
         if (daysLeft <= monthsdays[i + 1]) {
@@ -53,12 +56,6 @@ form.addEventListener('submit', (e) => {
             break;
         }
     }
-
-    console.log(`daysUser:${daysUser}`);
-
-    console.log(`QTD DE DIAS:${daysAge}`);
-    console.log(`QTD DE ANOS:${yearsAge}`);
-    console.log(`QTD DE MESES:${monthsAge}`);
 
     daysNumberText.innerText = daysAge;
     monthsNumberText.innerText = monthsAge;
